7abe0ad6115422e5b4b7766a0ae2fdcf2f3a4380
Anuj Sachan <anujsachan@gmail.com>
2018-07-10 13:32:13 +0530

    Add files via upload

---
+++ SpringMVCHibernate/src/main/java/com/anuj/spring/service/PersonService.java
@@ -0,0 +1,15 @@
+package com.anuj.spring.service;
+
+import java.util.List;
+
+import com.anuj.spring.model.Person;
+
+public interface PersonService {
+
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+	
+}
